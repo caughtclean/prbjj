c5d2a4fb35c51f2d7a43dd5a9ef23ca5db374ca2
Nathan <eyerleyn@gmail.com>
2017-10-06 10:34:37 +0000

    added the basic instructors content. Needs more styling

--- src/containers/Instructors/Instructors.js
+++ src/containers/Instructors/Instructors.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ImageButton, Index, Background } from './Instructors.style';
+import { Background, Img, P, Profile } from './Instructors.style';
 import { Container, Relative, Flex, marginBottom } from '../../theme/grid';
 import { A, Links } from '../../theme/types';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
@@ -10,6 +10,22 @@ class Projects extends Component {
       <Container>
         <Background/>
         <NavigationBar/>
+        <Profile>
+        <P>
+        <Img
+          src={require('../../assets/nick.jpg')}
+          alt="nick"/>
+        Since a very young age ​​Nicolas Ouellet has grown up within the martial arts. He currently holds a purple belt in brazilian jiu jitsu, a blue belt in judo and a black belt in karate. Nic is an active competitor with an excellent record and many podium finishes. Just about everyday you can find Nic on the mats drilling or helping others with their technique.
+          </P>
+        </Profile>
+         <Profile>
+        <P>
+        <Img
+          src={require('../../assets/raph.jpg')}
+          alt="raph"/>
+          Raph is also an active competitor who frequently finds himself on the podium. Raph is a high level blue belt who helps assist all classes.
+          </P>
+        </Profile>
       </Container>
     );
   }
--- src/containers/Instructors/Instructors.style.js
+++ src/containers/Instructors/Instructors.style.js
@@ -1,44 +1,6 @@
 import styled from 'styled-components';
 import media from '../../theme/media';
 
-export const ImageButton = styled.div`
-  cursor: pointer;
-  overflow: hidden;
-  & > img {
-    ${media.tablet`
-      width: 100vw;
-      height: 60vh;
-      `}
-    ${media.phone`
-       width: 100%;
-       height: 100%;
-      `}
-  }
-
-  & > img {
-  transition: transform .3s;
-  }
-  &:hover {
-    & > img {
-      transform: scale(1.3);
-    }
-  }
-`;
-
-export const Index = styled.div`
-  position = absolute;
-  left: 0;
-  bottom: 0;
-  font-size: 3em;
-  z-index: 1;
-  h1 {
-    margin: 0;
-    font-weight: bold;
-    opacity: 0.4;
-    transform: translateY(-20%);
-  }
-
-`
 
 export const Background = styled.div`
   position: fixed;
@@ -53,4 +15,20 @@ export const Background = styled.div`
   z-index: -99;
   opacity: 0.35;
 
+`;
+
+export const Img = styled.img`
+width: 50%;
+height: 50%;
+padding-right: 20px;
+
+`;
+
+export const P = styled.p`
+  float: right;
+  display: flex;
+`;
+
+export const Profile = styled.div`
+  margin-top: 20vh;
 `;
